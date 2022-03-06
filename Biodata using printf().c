9310d48f2de12f42db50f7bee55ff0de0b9b8baf
Keerthana J <98206044+itzz-keerthi@users.noreply.github.com>
2022-03-06 10:14:37 +0530

    Create your own biodata using printf()

---
+++ Biodata using printf().c
@@ -0,0 +1,10 @@
+/*TASK:
+Program to print our biodata using printf function*/
+#include<stdio.h>
+int main()
+{   
+    printf("******************** BIODATA ***********************\n");
+    printf("\nNAME: KEERTHANA J\nBIRTH DATE: 29/02/2004\nCOURSE NAME: B.E(COMPUTER SCIENCE AND ENGINEERING)\nAGE: 18 yrs\nCOLLEGE NAME: RAJALAKSHMI ENGINEERING COLLEGE\n");
+    printf("\n****************************************************");
+    return 0;
+}
